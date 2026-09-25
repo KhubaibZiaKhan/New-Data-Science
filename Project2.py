5e60c5f373576767a5e36e8485d7a2bf260b9c4a
KhubaibZiaKhan <khubaibking876@gmail.com>
2026-09-25 22:48:36 +0500

    Project2

--- Project2.py
+++ Project2.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('Titanic_Dataset.csv')
+data = pd.read_csv('Bestsellers with categories.csv')
 
 data.head(5)
 data.isnull().sum
