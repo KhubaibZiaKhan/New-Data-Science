820c0b3f1252bd7c6c69ee65b6daa38e93a5c69a
KhubaibZiaKhan <khubaibking876@gmail.com>
2026-09-26 14:17:06 +0500

    Project5

--- Project5.py
+++ Project5.py
@@ -14,5 +14,12 @@ CarsDF.columns
 sns.pairplot(CarsDF)
 plt.show()
 
+
 sns.heatmap(CarsDF.corr(),annot=True)
+plt.show()
+
+sns.boxplot(data=CarsDF,x='Price_in_thousands',y='Model')
+plt.show()
+
+sns.countplot(data=CarsDF,x='Model',hue='Fuel_Type')
 plt.show()
